1bd16db0eed914691eae591126331cbe11c932b7
Rafiya Ahmad <rafiyaahmed010@gmail.com>
2026-03-16 12:54:52 +0400

    Create insert_sample_data.sql

---
+++ SampleData/insert_sample_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.orders (order_id, FirstName, LastName, inserttime)
+VALUES
+(1, 'manish', 'tiwari', '2026-03-15T08:17:02.983'),
+(2, 'rani', 'sharma', '2026-03-15T08:17:02.987'),
+(3, 'yuvraj', 'verma', '2026-03-15T08:17:02.990'),
+(4, 'hema', 'kumar', '2026-03-15T15:03:08.327'),
+(5, 'Alice', 'singh', '2026-03-16T08:23:14.100');
